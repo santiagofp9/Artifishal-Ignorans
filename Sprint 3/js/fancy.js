db10ba885f56b459238a3abbfa8c56ea03f02f0c
elneto264 <57329411+elneto264@users.noreply.github.com>
2019-11-21 16:56:55 +0100

    Add files via upload

---
+++ Sprint 3/js/fancy.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+$(document).ready(function(){
+    
+    
+$( "input" ).click(function() {
+  $( "#logo2" ).hide();
+});
+ 
+$("input").click(function(){
+  $("#texto").show();
+});   
+         
+
+    
+    
+    
+	
+});
